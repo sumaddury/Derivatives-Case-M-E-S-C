ba7234f5f6b3ec5d010cb5e1a0defdfea5d8ac50
eric-zzhou <ericsuper@outlook.com>
2024-10-14 17:18:41 -0400

    Something was working at some point

--- eric_testing/yfnce_test.py
+++ eric_testing/yfnce_test.py
@@ -3,5 +3,14 @@ from datetime import datetime
 
 sp = yf.Ticker("^GSPC")
 # Get maximum period by the minute
-hist = sp.history(period="max", interval="1h")
+hist = sp.history(period="max", interval="1d")
+
+hist = hist.reset_index().rename(columns={"index": "Date"})
+hist = hist[["Date", "Open", "High", "Low", "Close", "Volume"]]
+hist["Date"] = hist["Date"].dt.tz_localize(None)
+hist = hist[
+    ((hist["Date"] >= datetime(1995, 1, 1)) & (hist["Date"] <= datetime(2006, 1, 1)))
+    | (hist["Date"] >= datetime(2009, 1, 1))
+]
+hist.to_csv("data/daily_under.csv")
 print(hist)
