2954db6314837a91aca1b47a841c10ef1fbce708
eric-zzhou <ericsuper@outlook.com>
2024-10-16 23:29:09 -0400

    eric strategy is up and running

--- eric_strategy.py
+++ eric_strategy.py
@@ -1,41 +1,27 @@
-import random
 import pandas as pd
+import numpy as np
 from datetime import datetime
 
 
 class Strategy:
-    def __init__(self) -> None:
-        self.capital = 100_000_000
-        self.portfolio_value = 0
+    def __init__(self, start_date, end_date) -> None:
+        self.capital: float = 100_000_000
+        self.portfolio_value: float = 0
+        self.spend_lim_ratio = 0.9
+        self.threshold = 20
 
-        self.start_date = datetime(2024, 1, 1)
-        self.end_date = datetime(2024, 3, 30)
+        self.start_date: datetime = start_date
+        self.end_date: datetime = end_date
 
-        self.options = pd.read_csv(
-            r"Derivatives-Case-M-E-S-C\data\cleaned_options_data.csv"
-        )
+        self.options: pd.DataFrame = pd.read_csv("data/cleaned_options_data.csv")
         self.options["datetime"] = pd.to_datetime(
             self.options["ts_recv"], format="%Y-%m-%dT%H:%M:%S.%fZ"
         )
-        parsed_features = self.options["symbol"].apply(self.parse_option_symbol)
-        # Create new columns by unpacking the parsed features
-        (
-            self.options["exp_date"],
-            self.options["action"],
-            self.options["strike_price"],
-        ) = zip(*parsed_features)
-        self.options = self.options[
-            [
-                "datetime",
-                "bid_px_00",
-                "ask_px_00",
-                "bid_sz_00",
-                "ask_sz_00",
-                "exp_date",
-                "action",
-                "strike_price",
-            ]
-        ]
+        symbol_parts = self.options["symbol"].str.split(" ", expand=True)[3]
+        self.options["exp_date"] = pd.to_datetime(symbol_parts.str[:6], format="%y%m%d")
+        self.options = self.options[self.options["exp_date"] <= self.end_date]
+        self.options["action"] = symbol_parts.str[6]
+        self.options["strike_price"] = symbol_parts.str[7:].astype(float) / 1000
         self.options = self.options.rename(
             columns={
                 "bid_px_00": "bidp",
@@ -44,54 +30,143 @@ class Strategy:
                 "ask_sz_00": "ask_sz",
             }
         )
-
-        self.underlying = pd.read_csv(
-            r"Derivatives-Case-M-E-S-C\data\underlying_data_hour.csv"
+        self.options["datetime"] = (
+            pd.to_datetime(self.options["datetime"])
+            .dt.tz_localize("UTC")
+            .dt.tz_convert("US/Eastern")
+            .dt.tz_localize(None)
         )
-        self.underlying.columns = self.underlying.columns.str.lower()
-        self.underlying["date"] = pd.to_datetime(self.underlying["date"])
-        print(self.underlying.columns)
-        print(self.underlying.head(5))
+        self.options["date"] = self.options["datetime"].dt.date
+        self.options["till_exp"] = (
+            self.options["exp_date"] - self.options["datetime"]
+        ).dt.days / 365.0
+        self.options["fair_value"] = (self.options["bidp"] + self.options["askp"]) / 2.0
 
-    # Define the function to parse a single option symbol
-    def parse_option_symbol(self, symbol):
-        """
-        example: SPX   240419C00800000
-        """
-        numbers = symbol.split(" ")[3]
-        exp_date = datetime.strptime(
-            numbers[:6], "%y%m%d"
-        )  # Convert to datetime object
-        action = numbers[6]  # Extract the action ('C' or 'P')
-        strike_price = float(numbers[7:]) / 1000  # Extract and convert the strike price
-        return [exp_date, action, strike_price]
+        self.underlying = pd.read_csv(r"data\spx_minute_level_data_jan_mar_2024.csv")
+        print(self.underlying.columns, flush=True)
+        self.underlying = self.underlying.loc[self.underlying["price"] > 0]
+        self.underlying["date"] = pd.to_datetime(
+            self.underlying["date"], format="%Y%m%d"
+        )
+        self.underlying_min = self.underlying.copy()
+
+        self.underlying = (
+            self.underlying.groupby("date")
+            .agg(
+                Open=("price", "first"),
+                High=("price", "max"),
+                Low=("price", "min"),
+                Close=("price", "last"),
+            )
+            .reset_index()
+        )
+        self.underlying_min["date"] = pd.to_datetime(
+            self.underlying_min["date"]
+        ).dt.date
+        time_part = pd.to_timedelta(self.underlying_min["ms_of_day"], unit="ms")
+        self.underlying_min["hour"] = time_part.dt.components.hours
+        self.underlying_min["minute"] = time_part.dt.components.minutes
+
+        indices = self.underlying["date"].searchsorted(self.options["datetime"]) - 1
+        indices = indices.clip(0, len(self.underlying) - 1)
+        self.options["underlying"] = self.underlying["Close"].iloc[indices].values
+        self.options["deviate"] = (
+            self.options["underlying"] - self.options["strike_price"]
+        )
+        self.options.loc[self.options["action"] == "P", "deviate"] *= -1
 
     def generate_orders(self) -> pd.DataFrame:
-        orders = []
-        num_orders = 1000
+        unique_exp_dates = self.options["exp_date"].unique()
+        orders = pd.DataFrame(
+            columns=["datetime", "option_symbol", "action", "order_size"]
+        )
 
-        for _ in range(num_orders):
-            row = self.options.sample(n=1).iloc[0]
-            action = random.choice(["B", "S"])
-
-            if action == "B":
-                order_size = random.randint(1, int(row["ask_sz_00"]))
-            else:
-                order_size = random.randint(1, int(row["bid_sz_00"]))
-
-            assert order_size <= int(row["ask_sz_00"]) or order_size <= int(
-                row["bid_sz_00"]
+        for exp_date in unique_exp_dates:
+            active_ord = pd.DataFrame(
+                columns=[
+                    "strike_price",
+                    "order_size",
+                    "datetime",
+                    "exp_date",
+                    "action",
+                ]
             )
+            spend_limit = self.capital * self.spend_lim_ratio
+            day_before_exp = (exp_date - pd.Timedelta(days=1)).date()
+            filtered_options = self.options[
+                (self.options["date"] == day_before_exp)
+                & (self.options["exp_date"] == exp_date)
+            ]
+            filtered_options["sort_by"] = (
+                filtered_options["deviate"] - filtered_options["askp"]
+            )
+            sorted_options = filtered_options.sort_values(by="sort_by", ascending=False)
 
-            order = {
-                "datetime": row["ts_recv"],
-                "option_symbol": row["symbol"],
-                "action": action,
-                "order_size": order_size,
-            }
-            orders.append(order)
+            # Loop through sorted options, keep buying the minimum between spend limit remaining and the ask size, break if sort_by < threshold
+            for _, option in sorted_options.iterrows():
+                # General stuff
+                if option["sort_by"] < self.threshold:
+                    break
+                if option["ask_sz"] == 0:
+                    continue
+                order_size = min(
+                    (self.capital - spend_limit) / (option["askp"] * 100),
+                    option["ask_sz"],
+                )
+                if order_size <= 0:
+                    break
+                options_cost = order_size * 100 * option["askp"]
+                self.capital -= options_cost + 0.5
 
-        return pd.DataFrame(orders)
+                new_row = pd.DataFrame(
+                    [
+                        {
+                            "datetime": option["ts_recv"],
+                            "option_symbol": option["symbol"],
+                            "action": "B",
+                            "order_size": order_size,
+                        }
+                    ]
+                )
+                new_row2 = pd.DataFrame(
+                    [
+                        {
+                            "strike_price": option["strike_price"],
+                            "order_size": order_size,
+                            "datetime": option["datetime"],
+                            "exp_date": option["exp_date"],
+                            "action": option["action"],
+                        }
+                    ]
+                )
+                orders = pd.concat(
+                    [orders, new_row],
+                    ignore_index=True,
+                )
+                active_ord = pd.concat(
+                    [active_ord, new_row2],
+                    ignore_index=True,
+                )
+            active_ord["date"] = pd.to_datetime(active_ord["exp_date"]).dt.date
+            active_ord["datetime"] = pd.to_datetime(active_ord["datetime"])
+            active_ord["hour"] = active_ord["datetime"].dt.hour
+            active_ord["minute"] = active_ord["datetime"].dt.minute
+            active_ord.loc[(active_ord["hour"] < 9), "hour"] = 9
+            active_ord.loc[
+                (active_ord["hour"] == 9) & (active_ord["minute"] < 31),
+                ["hour", "minute"],
+            ] = [9, 31]
+            active_ord.loc[(active_ord["hour"] > 16), ["hour", "minute"]] = [16, 0]
 
-
-s = Strategy()
+            active_ord = active_ord.merge(
+                self.underlying_min, how="left", on=["date", "hour", "minute"]
+            )
+            for _, option in active_ord.iterrows():
+                diff = option["price"] - option["strike_price"]
+                if option["action"] == "C":
+                    if diff > 0:
+                        self.capital += option["order_size"] * 100 * diff
+                else:
+                    if diff < 0:
+                        self.capital += option["order_size"] * 100 * -diff
+        return orders
